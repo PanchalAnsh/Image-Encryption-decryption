793986246858c4cf3b99a24f21cca65ca3dffef8
DESKTOP-C8UM0C1\Ansh Panchal <panchal.ansh83@gmail.com>
2023-04-11 13:27:32 +0530

    Image Encryption-Decryption Android Application

---
+++ IS_MiniProject/app/src/main/java/com/example/is_miniproject/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.is_miniproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+   ImageView imageEncrypt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageEncrypt = findViewById(R.id.img_encrypt);
+        imageEncrypt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent m2 = new Intent(MainActivity.this,PhotoCrypto.class);
+                startActivity(m2);
+            }
+        });
+
+    }
+}
